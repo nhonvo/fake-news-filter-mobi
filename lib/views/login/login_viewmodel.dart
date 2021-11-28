0516fd463d4df571283ac6e6426b50a249b965e5
Khánh Huỳnh Hữu <khanh201011@gmail.com>
2021-11-29 00:28:43 +0700

    Add login google, facebook

--- lib/views/login/login_viewmodel.dart
+++ lib/views/login/login_viewmodel.dart
@@ -6,7 +6,9 @@ import 'package:fake_news/resources/utils/app_routes.dart';
 import 'package:fake_news/resources/widgets/snackbar_custom.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginViewModel extends BaseViewModel {
@@ -46,6 +48,79 @@ class LoginViewModel extends BaseViewModel {
       await authRepo.saveAuthToken(user.token ?? '');
       Get.offAllNamed(Routes.HOME);
       EasyLoading.dismiss();
+    }
+  }
+
+  handleLoginFacebook() async {
+    final LoginResult result = await FacebookAuth.instance
+        .login(permissions: ['public_profile', 'email']); // by default we request the email and the public profile
+
+    if (result.status == LoginStatus.success) {
+      final AccessToken accessToken = result.accessToken!;
+
+      var response = await authApi.loginFacebook(accessToken.token);
+      if (response.isSuccessed == false) {
+        EasyLoading.dismiss();
+        snackBar(
+          'Error',
+          response.messages!,
+          'Something went wrong',
+          Icon(
+            Icons.error,
+            color: Colors.white,
+          ),
+          Colors.red,
+          Colors.white,
+          SnackPosition.BOTTOM,
+        );
+      } else {
+        LoginModel user = response.resultObj!;
+        await authRepo.saveAuthToken(user.token ?? '');
+        Get.offAllNamed(Routes.HOME);
+        EasyLoading.dismiss();
+      }
+    } else {
+      print(result.status);
+      print(result.message);
+    }
+  }
+
+  GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: [
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+      "https://www.googleapis.com/auth/userinfo.profile"
+    ],
+  );
+
+  Future<void> handleLoginGoogle() async {
+    try {
+      var result = await _googleSignIn.signIn();
+      GoogleSignInAuthentication googleSignInAuthentication = await result!.authentication;
+
+      var response = await authApi.loginGoogle(googleSignInAuthentication.idToken.toString());
+      if (response.isSuccessed == false) {
+        EasyLoading.dismiss();
+        snackBar(
+          'Error',
+          response.messages!,
+          'Something went wrong',
+          Icon(
+            Icons.error,
+            color: Colors.white,
+          ),
+          Colors.red,
+          Colors.white,
+          SnackPosition.BOTTOM,
+        );
+      } else {
+        LoginModel user = response.resultObj!;
+        await authRepo.saveAuthToken(user.token ?? '');
+        Get.offAllNamed(Routes.HOME);
+        EasyLoading.dismiss();
+      }
+    } catch (error) {
+      print(error);
     }
   }
 }
