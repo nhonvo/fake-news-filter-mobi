ee5808c0fefbe2c05c97cb8250ac2df8fd0ec767
Bui Phu Khuyen <khuyenpb@gmail.com>
2021-12-11 22:25:40 +0700

    Searching

---
+++ lib/views/searching/searching_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+
+class Searching extends StatefulWidget {
+  const Searching({Key? key}) : super(key: key);
+
+  @override
+  _SearchingState createState() => _SearchingState();
+}
+
+class _SearchingState extends State<Searching> {
+  @override
+  Widget build(BuildContext context) {
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+    return FloatingSearchBar(
+      automaticallyImplyBackButton: true,
+      hint: 'Search...',
+      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+      transitionDuration: const Duration(milliseconds: 800),
+      transitionCurve: Curves.easeInOut,
+      physics: const BouncingScrollPhysics(),
+      axisAlignment: isPortrait ? 0.0 : -1.0,
+      openAxisAlignment: 0.0,
+      width: isPortrait ? 600 : 500,
+      debounceDelay: const Duration(milliseconds: 500),
+      onQueryChanged: (query) {
+        // Call your model, bloc, controller here.
+      },
+      // Specify a custom transition to be used for
+      // animating between opened and closed stated.
+      transition: CircularFloatingSearchBarTransition(),
+      actions: [
+        FloatingSearchBarAction(
+          showIfOpened: false,
+          child: CircularButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ),
+        FloatingSearchBarAction.searchToClear(
+          showIfClosed: false,
+        ),
+      ],
+      builder: (context, transition) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Material(
+            color: Colors.white,
+            elevation: 4.0,
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              ListTile(
+                title: Text('Search'),
+                subtitle: Text('Content Search'),
+              )
+            ]),
+          ),
+        );
+      },
+    );
+  }
+}
