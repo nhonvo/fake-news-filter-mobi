b7587c1d88757d78c658b32d80b7e21cbd1f545e
Khanh Huu Huynh <khanh201011@gmail.com>
2021-12-17 09:57:22 +0000

    Update logic Fact News screen, localization

--- lib/resources/utils/app_helper.dart
+++ lib/resources/utils/app_helper.dart
@@ -1,20 +1,22 @@
+import 'package:get/get_utils/src/extensions/internacionalization.dart';
+
 class AppHelper {
   static String convertToAgo(DateTime input) {
     Duration diff = DateTime.now().difference(input);
     if (diff.inDays >= 1) {
       if (diff.inDays >= 31) {
-        return '${diff.inDays ~/ 31} months ago';
+        return '${diff.inDays ~/ 31} ${'months'.tr} ${'ago'.tr}';
       } else {
-        return '${diff.inDays} day(s) ago';
+        return '${diff.inDays} ${'days'.tr} ${'ago'.tr}';
       }
     } else if (diff.inHours >= 1) {
-      return '${diff.inHours} hour(s)';
+      return '${diff.inHours} ${'hours'.tr} ${'ago'.tr}';
     } else if (diff.inMinutes >= 1) {
-      return '${diff.inMinutes} minute(s)';
+      return '${diff.inMinutes} ${'minutes'.tr} ${'ago'.tr}';
     } else if (diff.inSeconds >= 1) {
-      return '${diff.inSeconds} second(s)';
+      return '${diff.inSeconds} ${'seconds'.tr} ${'ago'.tr}';
     } else {
-      return 'just now';
+      return '${'now'.tr}';
     }
   }
 }
